6f3d6d3d2a23906af0a94849cb9bbc10231d3635
Julianne <julivolotao@gmail.com>
2020-06-03 23:47:57 -0300

    [Frontend] Integração, busca de dados da API.

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,11 +1,86 @@
-import React from "react";
+import React, { useEffect, useState, ChangeEvent } from "react";
 import { Link } from "react-router-dom";
 
 import "./styles.scss";
 import logoImg from "../../assets/logo.svg";
 import { FiArrowLeft } from "react-icons/fi";
+import { Map, TileLayer, Marker } from "react-leaflet";
+
+import api from "../../services/api";
+import axios from "axios";
+
+interface Item {
+  id: number;
+  name: string;
+  image_url: string;
+}
+
+interface IBGEUFResponse {
+  sigla: string;
+}
+
+interface IBGECityResponse {
+  nome: string;
+}
 
 const CreatePoint = () => {
+  
+  // será armazenado um array de objetos, então se faz necessário indicar para cada
+  // atributo do objeto, o seu tipo
+  const [items, setItems] = useState<Item[]>([]);
+
+  // será armazenado um array de strings, podemos informar diretamente
+  const [ufs, setufs] = useState<string[]>([]);
+  const [cities, setCities] = useState<string[]>([]);
+
+  const [selectedUf, setSelectedUf] = useState("0");
+  const [selectedCity, setSelectedCity] = useState("0");
+
+  useEffect(() => {
+    api.get("items").then((res) => {
+      setItems(res.data);
+    });
+  }, []);
+
+  // retorna os estados
+  useEffect(() => {
+    axios
+      .get<IBGEUFResponse[]>(
+        "https://servicodados.ibge.gov.br/api/v1/localidades/estados"
+      )
+      .then((res) => {
+        const ufInicials = res.data.map((uf) => {
+          return uf.sigla;
+        });
+
+        setufs(ufInicials);
+      });
+  }, []);
+
+  // retorna as cidades de acordo com o UF selecionado
+  useEffect(() => {
+    axios
+      .get<IBGECityResponse[]>(
+        `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`
+      )
+      .then((res) => {
+        const cityNames = res.data.map((city) => {
+          return city.nome;
+        });
+
+        setCities(cityNames);
+      });
+  }, [selectedUf]);
+
+  const handleSelectUf = (event: ChangeEvent<HTMLSelectElement>) => {
+    const UF = event.target.value;
+    setSelectedUf(UF);
+  };
+  const handleSelectCity = (event: ChangeEvent<HTMLSelectElement>) => {
+    const City = event.target.value;
+    setSelectedCity(City);
+  };
+
   return (
     <>
       <div className="create__container">
@@ -23,7 +98,6 @@ const CreatePoint = () => {
               <h1>
                 Cadastro do <p>ponto de coleta</p>
               </h1>
-
 
               <fieldset className="form__box">
                 <legend>
@@ -48,32 +122,57 @@ const CreatePoint = () => {
                 </div>
               </fieldset>
 
-
               <fieldset className="form__box">
                 <legend>
                   <h2> Endereço </h2>
-                  <span> Selecione o endereço no mapa </span> 
+                  <span> Selecione o endereço no mapa </span>
                 </legend>
 
-                <div className="form__group">
+                <Map center={[-23.9417804, -46.3720246]} zoom={15}>
+                  <TileLayer
+                    attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  />
 
+                  <Marker position={[-23.9417804, -46.3720246]} />
+                </Map>
+
+                <div className="form__group">
                   <div className="form__field">
                     <label htmlFor="uf">Estado (UF)</label>
-                    <select name="uf" id="uf">
-                      <option value="0">Selecione um estado</option>
+                    <select
+                      name="uf"
+                      id="uf"
+                      onChange={handleSelectUf}
+                      value={selectedUf}
+                    >
+                      <option value="0">Selecione uma UF</option>
+                      {ufs.map((uf) => (
+                        <option key={uf} value={uf}>
+                          {uf}
+                        </option>
+                      ))}
                     </select>
                   </div>
 
                   <div className="form__field">
                     <label htmlFor="city">Cidade</label>
-                    <select name="city" id="city">
-                      <option value="0">Selecione um estado</option>
+                    <select 
+                      name="city" 
+                      id="city" 
+                      onChange={handleSelectCity}
+                      value={selectedCity}
+                    >
+                      <option value="0">Selecione uma cidade</option>
+                      {cities.map((city) => (
+                        <option key={city} value={city}>
+                          {city}
+                        </option>
+                      ))}
                     </select>
                   </div>
-
                 </div>
               </fieldset>
-
 
               <fieldset className="form__box">
                 <legend>
@@ -81,36 +180,19 @@ const CreatePoint = () => {
                 </legend>
 
                 <ul className="form__items">
-                  <li>
-                    <img src="http://localhost:3333/uploads/oleo.svg" alt="Óleo" />
-                    <span> óleo de cozinha </span>
-                  </li>
-                  <li className="selected">
-                    <img src="http://localhost:3333/uploads/oleo.svg" alt="Óleo" />
-                    <span> óleo de cozinha </span>
-                  </li>
-                  <li>
-                    <img src="http://localhost:3333/uploads/oleo.svg" alt="Óleo" />
-                    <span> óleo de cozinha </span>
-                  </li>
-                  <li>
-                    <img src="http://localhost:3333/uploads/oleo.svg" alt="Óleo" />
-                    <span> óleo de cozinha </span>
-                  </li>
-                  <li>
-                    <img src="http://localhost:3333/uploads/oleo.svg" alt="Óleo" />
-                    <span> óleo de cozinha </span>
-                  </li>
-                  <li>
-                    <img src="http://localhost:3333/uploads/oleo.svg" alt="Óleo" />
-                    <span> óleo de cozinha </span>
-                  </li>
+                  {items.map((item) => (
+                    <li key={item.id}>
+                      <img src={item.image_url} alt={item.name} />
+                      <span> {item.name} </span>
+                    </li>
+                  ))}
                 </ul>
               </fieldset>
 
-
-              <button className="form__submit" type="submit"> Cadastrar Ponto de coleta </button>
-
+              <button className="form__submit" type="submit">
+                {" "}
+                Cadastrar Ponto de coleta{" "}
+              </button>
             </form>
           </div>
         </div>
